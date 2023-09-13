8f6d6c2b7e376e5b53f0c65e3e106370daf438ce
HongzeXiao <124698379+HongzeXiao@users.noreply.github.com>
2023-09-13 17:02:27 -0400

    commit 2

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const { urlencoded } = require("body-parser");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 const port=3000;
@@ -22,10 +22,6 @@ const userSchema= new mongoose.Schema({
     email:String,
     password:String,
 });
-
-const secret=process.env.SECRET;
-
-userSchema.plugin(encrypt,{secret:secret ,encryptedFields:["password"] });
 
 const User= new mongoose.model("User",userSchema);
 
@@ -44,7 +40,7 @@ app.get('/register',(req,res)=>{
 app.post("/register",(req,res)=>{
     const newUser= new User({
         email:req.body.username,
-        password:req.body.password,
+        password:md5(req.body.password),
     })
 
     newUser.save().then(()=>{
@@ -58,7 +54,7 @@ app.post("/register",(req,res)=>{
 
 app.post("/login",(req,res)=>{
     const username=req.body.username;
-    const password=req.body.password;
+    const password=md5(req.body.password);
 
     User.findOne({email:username}).then((foundUser)=>{
         if (foundUser.password===password){
